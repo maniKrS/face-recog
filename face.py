a5bcda30b7cae8d3ace3bb55d2b7582a0401cb0d
maniKrS <111030106+maniKrS@users.noreply.github.com>
2022-09-19 20:15:54 -0700

    Add files via upload

---
+++ face.py
@@ -0,0 +1,11 @@
+import cv2
+img = cv2.imread('2.png')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+face_cascades = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+faces = face_cascades.detectMultiScale(gray,1.3,5)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+w),(255,0,0),2)
+cv2.imshow("img",img)
+cv2.waitKey(0)
